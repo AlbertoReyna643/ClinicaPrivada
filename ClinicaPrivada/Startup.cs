eb2f080348f83d42970571db5df89ec5f03544ba
alberto reyna martinez <albertormtz@outlook.com>
2018-10-07 12:23:12 -0500

    Add project files.

---
+++ ClinicaPrivada/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClinicaPrivada.Startup))]
+namespace ClinicaPrivada
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
